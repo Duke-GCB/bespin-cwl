4b5f4631bfb5f96a82c21f1f2252b9fcc7b4cf41
Dan Leehr <dan.leehr@duke.edu>
2017-03-27 10:16:21 -0400

    Update tee to catch stderr too and write to log dir

--- run_exomeseq.sh
+++ run_exomeseq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p data
+mkdir -p log
 date
-cwltool --debug --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json | tee exomeseq-$(date +"%Y%m%d-%k%M%S").log
+cwltool --debug --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json > >(tee log/exomeseq-out-$(date +"%Y%m%d-%k%M%S").log) 2> >(tee log/exomeseq-err-$(date +"%Y%m%d-%k%M%S").log >&2)
 date
