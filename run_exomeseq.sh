92f8d03271af58c6acbb28505d3ce839d94ddf5b
Dan Leehr <dan.leehr@duke.edu>
2017-03-27 10:11:40 -0400

    Add logging to exomeseq.sh

--- run_exomeseq.sh
+++ run_exomeseq.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data
 date
-cwltool --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json
+cwltool --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json | tee exomeseq-$(date +"%Y%m%d-%k%M%S").log
 date
