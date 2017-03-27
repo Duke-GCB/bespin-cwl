7fc47c63ebb2eea31877fa49dcec4b9ab0c10555
Dan Leehr <dan.leehr@duke.edu>
2017-03-27 10:12:44 -0400

    Raise to debug output in exomeseq.sh

--- run_exomeseq.sh
+++ run_exomeseq.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data
 date
-cwltool --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json | tee exomeseq-$(date +"%Y%m%d-%k%M%S").log
+cwltool --debug --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json | tee exomeseq-$(date +"%Y%m%d-%k%M%S").log
 date
