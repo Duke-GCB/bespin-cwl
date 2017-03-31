7ed0ce76c60b58e26bcc04ab85a816bea8cdac7a
Dan Leehr <dan.leehr@duke.edu>
2017-03-31 19:39:55 +0000

    Draft of scripts for base recalibrate
    - waiting on workflow to here to finish
    - Need to write AnalyzeCovariates cwl

---
+++ run_baserecalibrate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p data
+cwltool --outdir ./data community_workflows/tools/GATK-BaseRecalibrator.cwl samples/baserecalibrate-01-analyze-covariation.json
+cwltool --outdir ./data community_workflows/tools/GATK-BaseRecalibrator.cwl samples/baserecalibrate-02-covariation-after-recalibration.json
+cwltool --outdir ./data community_workflows/tools/GATK-AnalyzeCovariates.cwl samples/baserecalibrate-03-WRITE_ANALYZE_COVARIATES-generate-beforeafterplots.json
+cwltool --outdir ./data community_workflows/tools/GATK-PrintReads.cwl samples/baserecalibrate-04-PRINTREADS-apply-recalibration.json
+
