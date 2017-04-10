d0f44a79ed054d5355c4265351b0dd1c613846e7
Dan Leehr <dan.leehr@duke.edu>
2017-04-10 11:58:42 -0400

    Updates samples for baserecalibrate

--- run_baserecalibrate.sh
+++ run_baserecalibrate.sh
@@ -5,6 +5,6 @@ set -ex
 mkdir -p data
 cwltool --outdir ./data community-workflows/tools/GATK-BaseRecalibrator.cwl samples/baserecalibrate-01-analyze-covariation.json
 cwltool --outdir ./data community-workflows/tools/GATK-BaseRecalibrator.cwl samples/baserecalibrate-02-covariation-after-recalibration.json
-cwltool --outdir ./data community-workflows/tools/GATK-AnalyzeCovariates.cwl samples/baserecalibrate-03-WRITE_ANALYZE_COVARIATES-generate-beforeafterplots.json
-cwltool --outdir ./data community-workflows/tools/GATK-PrintReads.cwl samples/baserecalibrate-04-PRINTREADS-apply-recalibration.json
+cwltool --outdir ./data community-workflows/tools/GATK-AnalyzeCovariates.cwl samples/baserecalibrate-03-generate-plots.json
+cwltool --outdir ./data community-workflows/tools/GATK-PrintReads.cwl samples/baserecalibrate-04-apply-recalibration.json
 
