30d99abfc956fcfc62505bf26f7ecb14d0f989aa
Dan Leehr <dan.leehr@duke.edu>
2017-04-25 13:38:48 +0000

    Add virtualenv sourcing to run-toil-exomeseq-variantdiscovery

--- run-toil-exomeseq-variantdiscovery.sh
+++ run-toil-exomeseq-variantdiscovery.sh
@@ -6,5 +6,6 @@ LOGDIR=log/variant_discovery_toil
 mkdir -p $OUTDIR
 mkdir -p $LOGDIR
 date
+. /home/ubuntu/toil/bin/activate
 cwltoil --logDebug --outdir ./${OUTDIR} workflows/exomeseq-02-variantdiscovery.cwl samples/exomeseq-02-variantdiscovery.json > >(tee $LOGDIR/exomeseq-out-$(date +"%Y%m%d-%k%M%S").log) 2> >(tee $LOGDIR/exomeseq-err-$(date +"%Y%m%d-%k%M%S").log >&2)
 date
