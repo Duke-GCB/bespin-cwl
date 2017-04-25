eb2b8dbdb5fe8da1de1f81557f29216a50467208
Dan Leehr <dan.leehr@duke.edu>
2017-04-25 14:52:35 +0000

    Absolute path for output dirs, since toil didn't like file://./

--- run-toil-exomeseq-variantdiscovery.sh
+++ run-toil-exomeseq-variantdiscovery.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-OUTDIR=data/variant_discovery_toil
-LOGDIR=log/variant_discovery_toil
+OUTDIR=$(pwd)/data/variant_discovery_toil
+LOGDIR=$(pwd)/log/variant_discovery_toil
 
 mkdir -p $OUTDIR
 mkdir -p $LOGDIR
 date
 . /home/ubuntu/toil/bin/activate
-cwltoil --logDebug --outdir ./${OUTDIR} workflows/exomeseq-02-variantdiscovery.cwl samples/exomeseq-02-variantdiscovery.json > >(tee $LOGDIR/exomeseq-out-$(date +"%Y%m%d-%k%M%S").log) 2> >(tee $LOGDIR/exomeseq-err-$(date +"%Y%m%d-%k%M%S").log >&2)
+cwltoil --logDebug --outdir ${OUTDIR} workflows/exomeseq-02-variantdiscovery.cwl samples/exomeseq-02-variantdiscovery.json > >(tee $LOGDIR/exomeseq-out-$(date +"%Y%m%d-%k%M%S").log) 2> >(tee $LOGDIR/exomeseq-err-$(date +"%Y%m%d-%k%M%S").log >&2)
 date
