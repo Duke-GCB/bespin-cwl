32b8967547ce50a77a5b493940f19363def34328
Dan Leehr <dan.leehr@duke.edu>
2017-05-15 15:45:45 -0400

    Removes outdated examples

--- samples/exome-seq/run_exomeseq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p data
-mkdir -p log
-date
-cwltool --debug --outdir ./data workflows/exomeseq.cwl samples/exomeseq.json > >(tee log/exomeseq-out-$(date +"%Y%m%d-%k%M%S").log) 2> >(tee log/exomeseq-err-$(date +"%Y%m%d-%k%M%S").log >&2)
-date
--- samples/exome-seq/run_exomeseq_recalibrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p data/recalibrate
-mkdir -p log/recalibrate
-date
-cwltool --debug --outdir ./data/recalibrate workflows/exomeseq-recalibrate.cwl samples/exomeseq-recalibrate.json > >(tee log/recalibrate/exomeseq-out-$(date +"%Y%m%d-%k%M%S").log) 2> >(tee log/recalibrate/exomeseq-err-$(date +"%Y%m%d-%k%M%S").log >&2)
-date
