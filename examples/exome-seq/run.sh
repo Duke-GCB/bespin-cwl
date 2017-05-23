4bed7de3a1cc0ebdc4d9313b2d56c76bbffe3543
Dan Leehr <dan.leehr@duke.edu>
2017-05-23 17:19:42 +0000

    Updates run.sh script to use cachedir

--- examples/exome-seq/run.sh
+++ examples/exome-seq/run.sh
@@ -6,7 +6,7 @@
 WORKFLOW_NAME="$1"
 
 SCRIPT_DIR=$(dirname "$0")
-ROOT_DIR=${SCRIPT_DIR}/../..
+ROOT_DIR=$(readlink -f "${SCRIPT_DIR}/../..")
 RUN_DATE=$(date +"%Y%m%d-%k%M%S")
 
 WORKFLOWS_DIR=${ROOT_DIR}/workflows
@@ -16,20 +16,20 @@ LOG_ROOT=${ROOT_DIR}/log
 JOB_NAME="${WORKFLOW_NAME}-${RUN_DATE}"
 echo $JOB_NAME
 
-OUT_DIR=${DATA_ROOT}/${JOB_NAME}/
+OUT_DIR=${DATA_ROOT}/${WORKFLOW_NAME}/
+CACHE_DIR=${DATA_ROOT}/${WORKFLOW_NAME}-cache
 LOG_DIR=${LOG_ROOT}/${JOB_NAME}/
-TMP_DIR=${DATA_ROOT}/tmp/tmp
 
 mkdir -p $OUT_DIR
 mkdir -p $LOG_DIR
-echo "Starting on $(date)..." >(tee ${LOG_DIR}/${WORKFLOW_NAME}-err.log)
+mkdir -p $CACHE_DIR
+echo "Starting on $(date)..." >(tee ${LOG_DIR}/${JOB_NAME}-err.log)
 cwltool \
   --debug \
+  --cachedir ${CACHE_DIR} \
   --outdir ${OUT_DIR} \
-  --tmpdir-prefix ${TMP_DIR} \
-  --tmp-outdir-prefix ${TMP_DIR} \
   ${WORKFLOWS_DIR}/${WORKFLOW_NAME}.cwl \
   ${WORKFLOW_NAME}.json \
-  > >(tee ${LOG_DIR}/${WORKFLOW_NAME}-out.log) \
-  2> >(tee -a ${LOG_DIR}/${WORKFLOW_NAME}-err.log >&2)
-echo "Finished on $(date)..." >(tee -a ${LOG_DIR}/${WORKFLOW_NAME}-err.log)
+  > >(tee ${LOG_DIR}/${JOB_NAME}-out.log) \
+  2> >(tee -a ${LOG_DIR}/${JOB_NAME}-err.log >&2)
+echo "Finished on $(date)..." >(tee -a ${LOG_DIR}/${JOB_NAME}-err.log)
