9c7384d43065f79738abf0f8e4c9970f4075f43d
Dan Leehr <dan.leehr@duke.edu>
2017-07-21 18:39:03 +0000

    Updates jobstore and workdir locations in run-toil

--- examples/exome-seq/run-toil.sh
+++ examples/exome-seq/run-toil.sh
@@ -12,19 +12,23 @@ RUN_DATE=$(date +"%Y%m%d-%k%M%S")
 WORKFLOWS_DIR=${ROOT_DIR}/workflows
 DATA_ROOT=${ROOT_DIR}/data
 LOG_ROOT=${ROOT_DIR}/log
+JOBSTORE_ROOT=${DATA_ROOT}/jobstores
+
+mkdir -p $JOBSTORE_ROOT
 
 JOB_NAME="${WORKFLOW_NAME}-toil-${RUN_DATE}"
 echo $JOB_NAME
 
 OUT_DIR=${DATA_ROOT}/${JOB_NAME}/
 LOG_DIR=${LOG_ROOT}/${JOB_NAME}/
-JOBSTORE_DIR=$(mktemp -u -p ${DATA_ROOT}/tmp/)
+JOBSTORE_DIR=$(mktemp -u -p ${JOBSTORE_ROOT}/)
 WORK_DIR=$(mktemp -u -p ${DATA_ROOT}/work/)
 
 mkdir -p $OUT_DIR
 mkdir -p $LOG_DIR
+mkdir -p $WORK_DIR
 
-. /home/ubuntu/toil/bin/activate
+. /home/ubuntu/bespin-cwl/env-toil/bin/activate
 
 echo "Starting toil on $(date)..." >(tee ${LOG_DIR}/${WORKFLOW_NAME}-err.log)
 cwltoil \
