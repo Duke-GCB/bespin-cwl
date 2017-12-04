9860409051d50bb7b11dd50fb6a64b971ad5924e
Dan Leehr <dan.leehr@duke.edu>
2017-12-04 11:22:33 -0500

    Tweak build script to fail on error

--- scripts/build-packed-workflows.sh
+++ scripts/build-packed-workflows.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # Clear out any existing builds
-rm -rf build
-mkdir build
+OUTDIR="build"
+
+rm -rf ${OUTDIR}
+mkdir ${OUTDIR}
 
 for WORKFLOW in workflows/*.cwl; do
   BASE=$(basename $WORKFLOW)
-  cwltool --pack $WORKFLOW > build/${BASE}
+  cwltool --pack $WORKFLOW > ${OUTDIR}/${BASE}
 done
